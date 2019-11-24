9f3e0540f7b2eb480f7377de8ba7bd3adb5fe647
SivareddyUppathi <sivareddy.uppathi@gmail.com>
2019-11-24 13:30:48 +0530

    best resource for Spring Boot

---
+++ OriginServerSideTest-1/src/main/java/com/example/demo/OriginServerSideTest1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriginServerSideTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OriginServerSideTest1Application.class, args);
+	}
+
+}
